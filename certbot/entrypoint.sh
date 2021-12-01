fd4e264e3e4807f53985631cbe42d440e83b2a17
Sebastián Gálvez <72770565+sgalvezortiz@users.noreply.github.com>
2021-12-01 18:45:06 -0300

    Changed volume with host to volume between containers.

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -17,6 +17,10 @@ print-version)
     ;;
 
 init)
+    echo "Downloading recommended TLS parameters"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "/etc/letsencrypt/options-ssl-nginx.conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "/etc/letsencrypt/ssl-dhparams.pem"
+    echo
     echo "Generating dummy certificates"
     mkdir -p /etc/letsencrypt/live/ieai.aws.northeastern.edu
     openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
@@ -30,6 +34,18 @@ remove-cert)
     rm -Rf /etc/letsencrypt/live/ieai.aws.northeastern.edu && \
     rm -Rf /etc/letsencrypt/archive/ieai.aws.northeastern.edu && \
     rm -Rf /etc/letsencrypt/renewal/ieai.aws.northeastern.edu.conf
+    ;;
+
+create-cert-staging)
+    echo "Generating SSL certificates"
+    certbot certonly --webroot -w /var/www/certbot \
+    --email $email \
+    -d ieai.aws.northeastern.edu \
+    -d *.ieai.aws.northeastern.edu \
+    --rsa-key-size 4096 \
+    --agree-tos \
+    --force-renewal \
+    --staging
     ;;
 
 create-cert)

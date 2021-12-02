4c18ffe78c4d32b1fbbb68b2fd606befbc9d08d4
Sebastián Gálvez <72770565+sgalvezortiz@users.noreply.github.com>
2021-12-02 15:35:20 -0300

    Fix env variables on entrypoint.sh

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -7,80 +7,76 @@ then
     OPTS=$1
 fi
 
-# Disable staging mode here
-certbot_staging=$CERTBOT_STAGING
-certbot_email="$CERTBOT_EMAIL"
-
 case "$OPTS" in
 
-print-version)
-    echo "Certbot ready"
-    certbot --version
-    ;;
+  print-version)
+      echo "Certbot ready"
+      certbot --version
+      ;;
 
-init)
-    if [ ! -e "/etc/letsencrypt/options-ssl-nginx.conf" ] || [ ! -e "/etc/letsencrypt/ssl-dhparams.pem" ]; then
-      echo "### Downloading recommended TLS parameters ..."
-      curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "/etc/letsencrypt/options-ssl-nginx.conf"
-      curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "/etc/letsencrypt/ssl-dhparams.pem"
-      echo
-    fi
-    mkdir -p /etc/letsencrypt/live/ieai.aws.northeastern.edu
-    if [ ! -e "/etc/letsencrypt/live/ieai.aws.northeastern.edu/privkey.pem" ] || \
-       [ ! -e "/etc/letsencrypt/live/ieai.aws.northeastern.edu/fullchain.pem" ] ; then
-      echo "Generating dummy certificates"
-      openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
-      -keyout '/etc/letsencrypt/live/ieai.aws.northeastern.edu/privkey.pem' \
-      -out '/etc/letsencrypt/live/ieai.aws.northeastern.edu/fullchain.pem' \
-      -subj '/CN=ieai.aws.northeastern.edu'
-    else
-      echo "Initial certificates already are created. Please remove them with remove-cert command to re-generate them."
-    fi
-    ;;
+  init)
+      if [ ! -e "/etc/letsencrypt/options-ssl-nginx.conf" ] || [ ! -e "/etc/letsencrypt/ssl-dhparams.pem" ]; then
+        echo "### Downloading recommended TLS parameters ..."
+        curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "/etc/letsencrypt/options-ssl-nginx.conf"
+        curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "/etc/letsencrypt/ssl-dhparams.pem"
+        echo
+      fi
+      mkdir -p /etc/letsencrypt/live/ieai.aws.northeastern.edu
+      if [ ! -e "/etc/letsencrypt/live/ieai.aws.northeastern.edu/privkey.pem" ] || \
+         [ ! -e "/etc/letsencrypt/live/ieai.aws.northeastern.edu/fullchain.pem" ] ; then
+        echo "Generating dummy certificates"
+        openssl req -x509 -nodes -newkey rsa:4096 -days 1 \
+        -keyout '/etc/letsencrypt/live/ieai.aws.northeastern.edu/privkey.pem' \
+        -out '/etc/letsencrypt/live/ieai.aws.northeastern.edu/fullchain.pem' \
+        -subj '/CN=ieai.aws.northeastern.edu'
+      else
+        echo "Initial certificates already are created. First remove them with 'remove-cert' command to be able to re-generate them."
+      fi
+      ;;
 
-remove-cert)
-    echo "Deleting dummy certificates for ieai.aws.northeastern.edu"
-    if [ -d "/etc/letsencrypt/live/ieai.aws.northeastern.edu/" ] || \
-       [ -d "/etc/letsencrypt/archive/ieai.aws.northeastern.edu/" ] || \
-       [ -d "/etc/letsencrypt/renewal/ieai.aws.northeastern.edu/" ] ; then
-      rm -Rf /etc/letsencrypt/live/ieai.aws.northeastern.edu && \
-      rm -Rf /etc/letsencrypt/archive/ieai.aws.northeastern.edu && \
-      rm -Rf /etc/letsencrypt/renewal/ieai.aws.northeastern.edu.conf
-      echo "Done."
-    else
-      echo "Certificates folder does not exist. Aborting..."
-    fi
-    ;;
-create-cert)
-    echo "Generating SSL certificates"
-    if [ $certbot_email = "" ]; then echo "Error: missing variable CERTBOT_EMAIL" && exit 1; fi
-    if [ $certbot_staging != 0 ]; then staging_arg="--staging"; fi
-    certbot certonly --webroot -w /var/www/certbot \
-    --email $certbot_email \
-    -d robotreviewer.ieai.aws.northeastern.edu \
-    -d trialstreamer.ieai.aws.northeastern.edu \
-    --rsa-key-size 4096 \
-    --agree-tos \
-    --force-renewal \
-    --preferred-challenges http \
-    $staging_arg
-    ;;
+  remove-cert)
+      echo "Deleting dummy certificates for ieai.aws.northeastern.edu"
+      if [ -d "/etc/letsencrypt/live/ieai.aws.northeastern.edu/" ] || \
+         [ -d "/etc/letsencrypt/archive/ieai.aws.northeastern.edu/" ] || \
+         [ -d "/etc/letsencrypt/renewal/ieai.aws.northeastern.edu/" ] ; then
+        rm -Rf /etc/letsencrypt/live/ieai.aws.northeastern.edu && \
+        rm -Rf /etc/letsencrypt/archive/ieai.aws.northeastern.edu && \
+        rm -Rf /etc/letsencrypt/renewal/ieai.aws.northeastern.edu.conf
+        echo "Done."
+      else
+        echo "Certificates folder does not exist. Aborting..."
+      fi
+      ;;
+  create-cert)
+      echo "Generating SSL certificates"
+      if [ "$CERTBOT_EMAIL" = "" ]; then echo "Error: missing variable CERTBOT_EMAIL" && exit 1; fi
+      if [ "$CERTBOT_STAGING" != "0" ]; then staging_arg="--staging"; fi
+      certbot certonly --webroot -w /var/www/certbot \
+      --email "$CERTBOT_EMAIL" \
+      -d robotreviewer.ieai.aws.northeastern.edu \
+      -d trialstreamer.ieai.aws.northeastern.edu \
+      --rsa-key-size 4096 \
+      --agree-tos \
+      --force-renewal \
+      --preferred-challenges http \
+      $staging_arg
+      ;;
 
-renew)
-    echo "Renewing SSL certificates"
-    certbot renew
-    ;;
+  renew)
+      echo "Renewing SSL certificates"
+      certbot renew
+      ;;
 
-renew-loop)
-    /bin/sh -c 'trap exit TERM; while :; do certbot renew; sleep 12h & wait $${!}; done;'
-    ;;
-*)
-    if [ ! -z "$(which $1)" ]
-    then
-        $@
-    else
-        echo "Invalid command"
-        exit 1
-    fi
-    ;;
+  renew-loop)
+      /bin/sh -c 'trap exit TERM; while :; do certbot renew; sleep 12h & wait $${!}; done;'
+      ;;
+  *)
+      if [ ! -z "$(which $1)" ]
+      then
+          $@
+      else
+          echo "Invalid command"
+          exit 1
+      fi
+      ;;
 esac
